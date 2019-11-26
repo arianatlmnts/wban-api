dd371956ebf5c8538a395873765a9d4727a87d45
Arianatlmnts <ariana.talamantes@cetys.edu.mx>
2019-11-26 10:07:08 -0800

    changes

--- src/models/User.js
+++ src/models/User.js
@@ -6,8 +6,7 @@ const userSchema = new Schema({
     name: {
         type: String,
         required: true,
-        trim: true,
-        unique: true
+        trim: true
     },
     gender: {
         type: String,
